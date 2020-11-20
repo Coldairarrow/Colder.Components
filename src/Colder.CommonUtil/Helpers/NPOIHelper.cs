e5fbda9532aca43d2f09efe5cc95b767bac881db
Coldairarrow <862520575@qq.com>
2020-11-20 18:07:37 +0800

    NPOIHelper

--- src/Colder.CommonUtil/Helpers/NPOIHelper.cs
+++ src/Colder.CommonUtil/Helpers/NPOIHelper.cs
@@ -1,249 +1,188 @@
-﻿//using NPOI.HSSF.UserModel;
-//using NPOI.SS.UserModel;
-//using NPOI.XSSF.UserModel;
-//using System;
-//using System.Data;
-//using System.IO;
+﻿using System;
+using System.Data;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
-//namespace Colder.CommonUtil
-//{
-//    /// <summary>
-//    /// NPOI帮助类
-//    /// </summary>
-//    public static class NPOIHelper
-//    {
-//        /// <summary>
-//        /// 将excel导入到datatable
-//        /// </summary>
-//        /// <param name="filePath">excel路径</param>
-//        /// <param name="isColumnName">第一行是否是列名</param>
-//        /// <returns>返回datatable</returns>
-//        public static DataTable ReadExcel(string filePath, bool isColumnName)
-//        {
-//            DataTable dataTable = null;
-//            FileStream fs = null;
-//            DataColumn column = null;
-//            DataRow dataRow = null;
-//            IWorkbook workbook = null;
-//            ISheet sheet = null;
-//            IRow row = null;
-//            ICell cell = null;
-//            int startRow = 0;
+namespace Colder.CommonUtil
+{
+    /// <summary>
+    /// NPOI帮助类
+    /// </summary>
+    public static class NPOIHelper
+    {
+        /// <summary>
+        /// 将excel导入到datatable
+        /// 注：行号从0开始
+        /// </summary>
+        /// <param name="columnNameIndex">列名行号</param>
+        /// <param name="dataIndex">数据开始行号</param>
+        /// <param name="fileBytes">excel文件字节</param>
+        /// <param name="xlsx">是否为xlsx格式（即2007+版本）</param>
+        /// <returns>返回datatable</returns>
+        public static DataTable ReadExcel(byte[] fileBytes, int dataIndex = 1, int columnNameIndex = 0, bool xlsx = true)
+        {
+            using var fs = new MemoryStream(fileBytes);
 
-//            using (fs = File.OpenRead(filePath))
-//            {
-//                // 2007版本
-//                if (filePath.IndexOf(".xlsx") > 0)
-//                {
-//                    workbook = new XSSFWorkbook(fs);
-//                }
-//                // 2003版本
-//                else if (filePath.IndexOf(".xls") > 0)
-//                {
-//                    workbook = new HSSFWorkbook(fs);
-//                }
+            IWorkbook workbook = xlsx ? (IWorkbook)new XSSFWorkbook(fs) : new HSSFWorkbook(fs);
 
-//                if (workbook != null)
-//                {
-//                    sheet = workbook.GetSheetAt(0);//读取第一个sheet，当然也可以循环读取每个sheet
-//                    dataTable = new DataTable();
-//                    if (sheet != null)
-//                    {
-//                        int rowCount = sheet.LastRowNum;//总行数
-//                        if (rowCount > 0)
-//                        {
-//                            IRow firstRow = sheet.GetRow(0);//第一行
-//                            int cellCount = firstRow.LastCellNum;//列数
+            if (workbook == null)
+            {
+                throw new Exception("读取失败");
+            }
 
-//                            //构建datatable的列
-//                            if (isColumnName)
-//                            {
-//                                startRow = 1;//如果第一行是列名，则从第二行开始读取
-//                                for (int i = firstRow.FirstCellNum; i < cellCount; ++i)
-//                                {
-//                                    cell = firstRow.GetCell(i);
-//                                    if (cell != null)
-//                                    {
-//                                        if (cell.StringCellValue != null)
-//                                        {
-//                                            column = new DataColumn(cell.StringCellValue);
-//                                            dataTable.Columns.Add(column);
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                            else
-//                            {
-//                                for (int i = firstRow.FirstCellNum; i < cellCount; ++i)
-//                                {
-//                                    column = new DataColumn("column" + (i + 1));
-//                                    dataTable.Columns.Add(column);
-//                                }
-//                            }
+            var sheet = workbook.GetSheetAt(0);
+            var dataTable = new DataTable();
 
-//                            //填充行
-//                            for (int i = startRow; i <= rowCount; ++i)
-//                            {
-//                                row = sheet.GetRow(i);
-//                                if (row == null)
-//                                {
-//                                    continue;
-//                                }
+            if (sheet == null)
+            {
+                throw new Exception("读取失败");
+            }
 
-//                                dataRow = dataTable.NewRow();
-//                                for (int j = row.FirstCellNum; j < cellCount; ++j)
-//                                {
-//                                    cell = row.GetCell(j);
-//                                    if (cell == null)
-//                                    {
-//                                        dataRow[j] = "";
-//                                    }
-//                                    else
-//                                    {
-//                                        //CellType(Unknown = -1,Numeric = 0,String = 1,Formula = 2,Blank = 3,Boolean = 4,Error = 5,)
-//                                        switch (cell.CellType)
-//                                        {
-//                                            case CellType.Blank:
-//                                                dataRow[j] = "";
-//                                                break;
-//                                            case CellType.Numeric:
-//                                                short format = cell.CellStyle.DataFormat;
-//                                                //对时间格式（2015.12.5、2015/12/5、2015-12-5等）的处理
-//                                                if (format == 14 || format == 31 || format == 57 || format == 58)
-//                                                {
-//                                                    dataRow[j] = cell.DateCellValue;
-//                                                }
-//                                                else
-//                                                {
-//                                                    dataRow[j] = cell.NumericCellValue;
-//                                                }
+            int rowCount = sheet.LastRowNum;//总行数
 
-//                                                break;
-//                                            case CellType.String:
-//                                                dataRow[j] = cell.StringCellValue;
-//                                                break;
-//                                        }
-//                                    }
-//                                }
-//                                dataTable.Rows.Add(dataRow);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            return dataTable;
+            if (rowCount == 0)
+            {
+                throw new Exception("无数据");
+            }
 
+            IRow columnNameRow = sheet.GetRow(columnNameIndex);//列名行
+            int cellCount = columnNameRow.LastCellNum;//列数
 
-//        }
+            ICell cell;
+            DataColumn column;
+            //构建datatable的列
+            if (columnNameIndex >= 0)
+            {
+                for (int i = columnNameRow.FirstCellNum; i < cellCount; ++i)
+                {
+                    cell = columnNameRow.GetCell(i);
+                    if (cell != null)
+                    {
+                        if (cell.StringCellValue != null)
+                        {
+                            column = new DataColumn(cell.StringCellValue);
+                            dataTable.Columns.Add(column);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = columnNameRow.FirstCellNum; i < cellCount; ++i)
+                {
+                    column = new DataColumn("column" + (i + 1));
+                    dataTable.Columns.Add(column);
+                }
+            }
 
-//        /// <summary>
-//        /// 写入excel
-//        /// </summary>
-//        /// <param name="dt">datatable</param>
-//        /// <param name="strFile">strFile</param>
-//        /// <returns></returns>
-//        public static bool DataTableToExcel(DataTable dt, string strFile)
-//        {
-//            bool result = false;
-//            IWorkbook workbook = null;
-//            FileStream fs = null;
-//            IRow row = null;
-//            ISheet sheet = null;
-//            ICell cell = null;
-//            try
-//            {
-//                if (dt != null && dt.Rows.Count > 0)
-//                {
-//                    workbook = new HSSFWorkbook();
-//                    sheet = workbook.CreateSheet("Sheet0");//创建一个名称为Sheet0的表
-//                    int rowCount = dt.Rows.Count;//行数
-//                    int columnCount = dt.Columns.Count;//列数
+            //填充行
+            for (int i = dataIndex; i <= rowCount; ++i)
+            {
+                var row = sheet.GetRow(i);
+                if (row == null)
+                {
+                    continue;
+                }
 
-//                    //设置列头
-//                    row = sheet.CreateRow(0);//excel第一行设为列头
-//                    for (int c = 0; c < columnCount; c++)
-//                    {
-//                        cell = row.CreateCell(c);
-//                        cell.SetCellValue(dt.Columns[c].ColumnName);
-//                    }
+                var dataRow = dataTable.NewRow();
+                for (int j = row.FirstCellNum; j < cellCount; ++j)
+                {
+                    cell = row.GetCell(j);
+                    if (cell == null)
+                    {
+                        dataRow[j] = "";
+                    }
+                    else
+                    {
+                        //CellType(Unknown = -1,Numeric = 0,String = 1,Formula = 2,Blank = 3,Boolean = 4,Error = 5,)
+                        switch (cell.CellType)
+                        {
+                            case CellType.Blank:
+                                dataRow[j] = "";
+                                break;
+                            case CellType.Numeric:
+                                short format = cell.CellStyle.DataFormat;
+                                //对时间格式（2015.12.5、2015/12/5、2015-12-5等）的处理
+                                if (format == 14 || format == 31 || format == 57 || format == 58)
+                                {
+                                    dataRow[j] = cell.DateCellValue;
+                                }
+                                else
+                                {
+                                    dataRow[j] = cell.NumericCellValue;
+                                }
 
-//                    //设置每行每列的单元格,
-//                    for (int i = 0; i < rowCount; i++)
-//                    {
-//                        row = sheet.CreateRow(i + 1);
-//                        for (int j = 0; j < columnCount; j++)
-//                        {
-//                            cell = row.CreateCell(j);//excel第二行开始写入数据
-//                            cell.SetCellValue(dt.Rows[i][j].ToString());
-//                        }
-//                    }
-//                    using (fs = File.OpenWrite(strFile))
-//                    {
-//                        workbook.Write(fs);//向打开的这个xls文件中写入数据
-//                        result = true;
-//                    }
-//                }
-//                return result;
-//            }
-//            catch (Exception ex)
-//            {
-//                if (fs != null)
-//                {
-//                    fs.Close();
-//                }
-//                return false;
-//            }
-//        }
+                                break;
+                            case CellType.String:
+                                dataRow[j] = cell.StringCellValue;
+                                break;
+                        }
+                    }
+                }
+                dataTable.Rows.Add(dataRow);
+            }
 
-//        /// <summary>
-//        /// Datable导出成Excel
-//        /// </summary>
-//        /// <param name="dt"></param>
-//        /// <param name="file">导出路径(包括文件名与扩展名)</param>
-//        public static void TableToExcel(DataTable dt, string file)
-//        {
-//            IWorkbook workbook;
-//            string fileExt = Path.GetExtension(file).ToLower();
-//            if (fileExt == ".xlsx")
-//            { workbook = new XSSFWorkbook(); }
-//            else if (fileExt == ".xls")
-//            { workbook = new HSSFWorkbook(); }
-//            else
-//            { workbook = null; }
-//            if (workbook == null)
-//            { return; }
-//            ISheet sheet = string.IsNullOrEmpty(dt.TableName) ? workbook.CreateSheet("Sheet1") : workbook.CreateSheet(dt.TableName);
+            return dataTable;
+        }
 
-//            //表头
-//            IRow row = sheet.CreateRow(0);
-//            for (int i = 0; i < dt.Columns.Count; i++)
-//            {
-//                ICell cell = row.CreateCell(i);
-//                cell.SetCellValue(dt.Columns[i].ColumnName);
-//            }
+        /// <summary>
+        /// 写入excel
+        /// </summary>
+        /// <param name="dt">数据</param>
+        /// <param name="xlsx">是否为xlsx格式（即2007+版本）</param>
+        /// <returns>excel字节</returns>
+        public static byte[] WriteExcel(DataTable dt, bool xlsx = true)
+        {
+            using MemoryStream stream = new MemoryStream();
 
-//            //数据
-//            for (int i = 0; i < dt.Rows.Count; i++)
-//            {
-//                IRow row1 = sheet.CreateRow(i + 1);
-//                for (int j = 0; j < dt.Columns.Count; j++)
-//                {
-//                    ICell cell = row1.CreateCell(j);
-//                    cell.SetCellValue(dt.Rows[i][j].ToString());
-//                }
-//            }
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                IWorkbook workbook = xlsx ? (IWorkbook)new XSSFWorkbook() : new HSSFWorkbook();
+                var sheet = workbook.CreateSheet("Sheet0");
+                int rowCount = dt.Rows.Count;//行数
+                int columnCount = dt.Columns.Count;//列数
 
-//            //转为字节数组
-//            MemoryStream stream = new MemoryStream();
-//            workbook.Write(stream);
-//            var buf = stream.ToArray();
+                //设置列头
+                var row = sheet.CreateRow(0);
+                ICell cell;
+                for (int c = 0; c < columnCount; c++)
+                {
+                    cell = row.CreateCell(c);
+                    cell.SetCellValue(dt.Columns[c].ColumnName);
+                }
 
-//            //保存为Excel文件
-//            using (FileStream fs = new FileStream(file, FileMode.Create, FileAccess.Write))
-//            {
-//                fs.Write(buf, 0, buf.Length);
-//                fs.Flush();
-//            }
-//        }
-//    }
-//}
+                //设置每行每列的单元格,
+                for (int i = 0; i < rowCount; i++)
+                {
+                    row = sheet.CreateRow(i + 1);
+                    for (int j = 0; j < columnCount; j++)
+                    {
+                        cell = row.CreateCell(j);//excel第二行开始写入数据
+                        cell.SetCellValue(dt.Rows[i][j].ToString());
+                    }
+                }
+
+                workbook.Write(stream);
+            }
+
+            return stream.ToArray();
+        }
+
+        /// <summary>
+        /// 通过模板写入excel
+        /// </summary>
+        /// <param name="dt">数据</param>
+        /// <param name="xlsx">是否为xlsx格式（即2007+版本）</param>
+        /// <returns>excel字节</returns>
+        public static byte[] WriteExcelByTemplate(DataTable dt, bool xlsx = true)
+        {
+            //TODO 待实现，实现方式可参考Aspose.cells
+
+            using MemoryStream stream = new MemoryStream();
+
+            return stream.ToArray();
+        }
+    }
+}
