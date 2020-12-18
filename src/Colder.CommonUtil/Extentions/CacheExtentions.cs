57a130f86b8ea84ceb08d7038aff28a605cb832e
Coldairarrow <862520575@qq.com>
2020-12-18 20:39:30 +0800

    缓存封装

---
+++ src/Colder.CommonUtil/Extentions/CacheExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Colder.CommonUtil
+{
+    public static class CacheExtentions
+    {
+        public static async Task<T> GetOrSet<T>(this IDistributedCache distributedCache, string cacheKey, Func<Task<T>> getFromDb)
+        {
+            var cache = await distributedCache.GetObjectAsync<T>(cacheKey);
+            if (cache.IsNullOrEmpty() || cache.Equals(default(T)))
+            {
+                cache = await getFromDb();
+                await distributedCache.SetObjectAsync(cacheKey, cache);
+            }
+
+            return cache;
+        }
+    }
+}
