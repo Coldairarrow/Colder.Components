c155e061864fb3ac3a2ded1198cc69bc6972d96a
Coldairarrow <862520575@qq.com>
2021-01-13 15:14:12 +0800

    分布式锁内存实现

---
+++ src/Colder.DistributedLock.Abstractions/IDistributedLock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Colder.DistributedLock.Abstractions
+{
+    /// <summary>
+    /// 分布式锁
+    /// </summary>
+    public interface IDistributedLock
+    {
+        /// <summary>
+        /// 获取锁(类似于lock(obj))
+        /// </summary>
+        /// <param name="key">锁键值</param>
+        /// <param name="timeout">超时时间</param>
+        /// <returns></returns>
+        Task<IDisposable> Lock(string key, TimeSpan? timeout = null);
+    }
+}
---
+++ src/Colder.DistributedLock.InMemory/InMemoryDistributedLock.cs
@@ -0,0 +1,70 @@
+﻿using Colder.DistributedLock.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Colder.DistributedLock.InMemory
+{
+    internal class InMemoryDistributedLock : IDistributedLock
+    {
+        public InMemoryDistributedLock(IMemoryCache memoryCache)
+        {
+            LockDic = memoryCache;
+        }
+        public IMemoryCache LockDic { get; }
+        private readonly ConcurrentDictionary<string, object> _cacheLock
+            = new ConcurrentDictionary<string, object>();
+        public Task<IDisposable> Lock(string key, TimeSpan? timeout)
+        {
+            lock (_cacheLock.GetOrAdd(key, new object()))
+            {
+                var theLock = LockDic.GetOrCreate(key, cacheEntry =>
+                {
+                    var newLock = new SemaphoreSlimLock();
+
+                    cacheEntry.SlidingExpiration = TimeSpan.FromMinutes(1);
+                    cacheEntry.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration
+                    {
+                        EvictionCallback = (_, _, _, _) =>
+                        {
+                            _cacheLock.TryRemove(key, out object _);
+                            newLock.DisposeLock();
+                        }
+                    });
+
+                    return newLock;
+                });
+
+                theLock.WaitOne(timeout);
+
+                return Task.FromResult((IDisposable)theLock);
+            }
+        }
+
+        private class SemaphoreSlimLock : IDisposable
+        {
+            private readonly Semaphore _semaphore = new Semaphore(1, 1);
+            public void WaitOne(TimeSpan? timeout)
+            {
+                if (timeout == null)
+                {
+                    _semaphore.WaitOne();
+                }
+                else
+                {
+                    _semaphore.WaitOne(timeout.Value);
+                }
+            }
+            public void Dispose()
+            {
+                _semaphore.Release();
+            }
+            public void DisposeLock()
+            {
+                _semaphore.Dispose();
+            }
+        }
+    }
+}
