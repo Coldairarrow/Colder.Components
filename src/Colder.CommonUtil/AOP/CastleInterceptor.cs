edf7a244eaff660eabe8a3e248a63a3fed260295
Coldairarrow <862520575@qq.com>
2021-01-21 14:54:17 +0800

    aop修复

--- src/Colder.CommonUtil/AOP/CastleInterceptor.cs
+++ src/Colder.CommonUtil/AOP/CastleInterceptor.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -21,46 +20,40 @@ namespace Colder.CommonUtil
         }
 
         private List<BaseAOPAttribute> _aops;
-        private int _depth;
-        private readonly ConcurrentDictionary<int, IAOPContext> _contextDic = new ConcurrentDictionary<int, IAOPContext>();
-        private readonly ConcurrentDictionary<int, long> _timeDic = new ConcurrentDictionary<int, long>();
+        private IAOPContext _context;
+        private long _beginTime;
 
         private async Task Befor()
         {
-            _timeDic[_depth] = Stopwatch.GetTimestamp();
+            _beginTime = Stopwatch.GetTimestamp();
 
             foreach (var aAop in _aops)
             {
-                await aAop.Befor(_contextDic[_depth]);
+                await aAop.Befor(_context);
             }
         }
         private async Task After()
         {
             var elapsedMilliseconds = new TimeSpan((long)(TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency
-                * (Stopwatch.GetTimestamp() - _timeDic[_depth]))).TotalMilliseconds;
+                * (Stopwatch.GetTimestamp() - _beginTime))).TotalMilliseconds;
 
-            var context = _contextDic[_depth];
             //记录耗时方法
             if (elapsedMilliseconds >= _minElapsedMilliseconds)
             {
                 var logger = _serviceProvider.GetService<ILoggerFactory>()?.CreateLogger(GetType());
 
                 logger?.LogInformation("执行方法 {InvokeMethod} 耗时 {ElapsedMilliseconds:N}ms",
-                    $"{context.TargetType?.Name}.{context.Method.Name}", elapsedMilliseconds);
+                    $"{_context.TargetType?.Name}.{_context.Method.Name}", elapsedMilliseconds);
             }
 
             foreach (var aAop in _aops)
             {
-                await aAop.After(context);
+                await aAop.After(_context);
             }
-
-            _depth--;
         }
         private void Init(IInvocation invocation)
         {
-            _depth++;
-
-            _contextDic[_depth] = new CastleAOPContext(invocation, _serviceProvider);
+            _context = new CastleAOPContext(invocation, _serviceProvider);
 
             _aops = invocation.MethodInvocationTarget.GetCustomAttributes(typeof(BaseAOPAttribute), true)
                 .Concat(invocation.InvocationTarget.GetType().GetCustomAttributes(typeof(BaseAOPAttribute), true))
