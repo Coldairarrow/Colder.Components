1b8bfc0cb3b04e7b54d6daa54049f05db9a267ec
Coldairarrow <862520575@qq.com>
2021-02-01 16:30:07 +0800

    内存锁优化

--- src/Colder.DistributedLock.InMemory/InMemoryDistributedLock.cs
+++ src/Colder.DistributedLock.InMemory/InMemoryDistributedLock.cs
@@ -1,25 +1,32 @@
-﻿using System;
+﻿using Colder.DistributedLock.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
-using Colder.DistributedLock.Abstractions;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace Colder.DistributedLock.InMemory
 {
     internal class InMemoryDistributedLock : IDistributedLock
     {
         private readonly IMemoryCache _lockDic = new MemoryCache(new MemoryCacheOptions());
+        private readonly ConcurrentDictionary<string, SemaphoreSlim> _keyDic
+            = new ConcurrentDictionary<string, SemaphoreSlim>();
         public async Task<IDisposable> Lock(string key, TimeSpan? timeout)
         {
-            timeout = timeout ?? TimeSpan.FromSeconds(10);
-
-            SemaphoreSlimLock theLock;
-
-            lock (key)
+            SemaphoreSlim keyLock = null;
+            try
             {
+                timeout = timeout ?? TimeSpan.FromSeconds(10);
+
+                UsingLock theLock;
+
+                keyLock = _keyDic.GetOrAdd(key, new SemaphoreSlim(1, 1));
+                await keyLock.WaitAsync(TimeSpan.FromSeconds(10));
+
                 theLock = _lockDic.GetOrCreate(key, cacheEntry =>
                 {
-                    var newLock = new SemaphoreSlimLock();
+                    var newLock = new UsingLock();
 
                     cacheEntry.SlidingExpiration = TimeSpan.FromMinutes(10);
                     cacheEntry.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration
@@ -27,19 +34,29 @@ namespace Colder.DistributedLock.InMemory
                         EvictionCallback = (_, _, _, _) =>
                         {
                             newLock.DisposeLock();
+                            _keyDic.TryRemove(key, out _);
+                            keyLock.Dispose();
                         }
                     });
 
                     return newLock;
                 });
+
+                keyLock.Release();
+
+                await theLock.Wait(timeout.Value);
+
+                return theLock;
             }
+            catch
+            {
+                keyLock?.Release();
 
-            await theLock.Wait(timeout.Value);
-
-            return theLock;
+                throw new Exception($"获取锁失败 Key:{key}");
+            }
         }
 
-        private class SemaphoreSlimLock : IDisposable
+        private class UsingLock : IDisposable
         {
             private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
             public async Task Wait(TimeSpan timeout)
