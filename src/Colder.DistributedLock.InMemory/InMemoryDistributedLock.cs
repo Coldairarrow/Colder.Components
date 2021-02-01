63fac11a52d3377991176926c3a27caf98e113ce
Coldairarrow <862520575@qq.com>
2021-02-01 13:00:34 +0800

    内存锁优化

--- src/Colder.DistributedLock.InMemory/InMemoryDistributedLock.cs
+++ src/Colder.DistributedLock.InMemory/InMemoryDistributedLock.cs
@@ -1,15 +1,15 @@
-﻿using Colder.DistributedLock.Abstractions;
-using Microsoft.Extensions.Caching.Memory;
-using System;
+﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Colder.DistributedLock.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Colder.DistributedLock.InMemory
 {
     internal class InMemoryDistributedLock : IDistributedLock
     {
         private readonly IMemoryCache _lockDic = new MemoryCache(new MemoryCacheOptions());
-        public Task<IDisposable> Lock(string key, TimeSpan? timeout)
+        public async Task<IDisposable> Lock(string key, TimeSpan? timeout)
         {
             timeout = timeout ?? TimeSpan.FromSeconds(10);
 
@@ -34,17 +34,17 @@ namespace Colder.DistributedLock.InMemory
                 });
             }
 
-            theLock.WaitOne(timeout.Value);
+            await theLock.Wait(timeout.Value);
 
-            return Task.FromResult((IDisposable)theLock);
+            return theLock;
         }
 
         private class SemaphoreSlimLock : IDisposable
         {
-            private readonly Semaphore _semaphore = new Semaphore(1, 1);
-            public void WaitOne(TimeSpan timeout)
+            private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+            public async Task Wait(TimeSpan timeout)
             {
-                if (!_semaphore.WaitOne(timeout))
+                if (!await _semaphore.WaitAsync(timeout))
                 {
                     throw new Exception("获取锁超时");
                 }
