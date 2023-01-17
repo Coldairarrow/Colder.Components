7d7165008e4d57bb3204e4ad374d755f4c95d7df
sealoyal <sealoyal@hotmail.com>
2023-01-17 10:14:24 +0800

    支持可配置日志输出模板

---
+++ src/Colder.Logging.Abstractions/OutputOption.cs
@@ -0,0 +1,12 @@
+﻿namespace Colder.Logging.Abstractions;
+
+/// <summary>
+/// 带模板输出日志
+/// </summary>
+public class OutputOption: EnableOption
+{
+    /// <summary>
+    /// 日志模板配置
+    /// </summary>
+    public string Template { get; set; } = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3} {SourceContext:l}] {Message:lj}{NewLine}{Exception}";
+}
