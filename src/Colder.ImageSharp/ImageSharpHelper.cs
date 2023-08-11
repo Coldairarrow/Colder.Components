140f5431bfdf9fe6195ebe4c5a08e52c6ddc5d2a
Coldairarrow <862520575@qq.com>
2023-08-11 21:05:14 +0800

    增加ImageSharpHelper、PdfHelper

---
+++ src/Colder.ImageSharp/ImageSharpHelper.cs
@@ -0,0 +1,38 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+
+namespace Colder.ImageSharp;
+
+/// <summary>
+///
+/// </summary>
+public static class ImageSharpHelper
+{
+    /// <summary>
+    /// 等比压缩图片并转为jpg
+    /// </summary>
+    /// <param name="imageBytes"></param>
+    /// <param name="width"></param>
+    /// <returns></returns>
+    public static byte[] CompressImage(byte[] imageBytes, int width = 1080)
+    {
+        var image = Image.Load(imageBytes);
+        image.Metadata.ExifProfile = null;
+
+        if (image.Width > width)
+        {
+            var rate = (double)width / image.Width;
+            image.Mutate(x => x.Resize((int)(image.Width * rate), (int)(image.Height * rate)));
+        }
+
+        var ms = new MemoryStream();
+        image.SaveAsJpeg(ms, new JpegEncoder()
+        {
+            Quality = 75
+        });
+
+        return ms.ToArray();
+    }
+}
---
+++ src/Colder.Pdf/PdfHelper.cs
@@ -0,0 +1,40 @@
+﻿using PDFiumSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+
+namespace Colder.Pdf;
+
+/// <summary>
+/// 
+/// </summary>
+public static class PdfHelper
+{
+    /// <summary>
+    /// pdf转jpg图片
+    /// </summary>
+    /// <param name="pdfBytes"></param>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <returns></returns>
+    public static byte[] Pdf2Jpg(byte[] pdfBytes, int width = 1280, int height = 850)
+    {
+        using var pdfDocument = new PdfDocument(pdfBytes);
+        var firstPage = pdfDocument.Pages[0];
+
+        using var pageBitmap = new PDFiumBitmap(width, height, true);
+
+        firstPage.Render(pageBitmap);
+
+        var image = Image.Load(pageBitmap.AsBmpStream());
+
+        // Set the background to white, otherwise it's black. https://github.com/SixLabors/ImageSharp/issues/355#issuecomment-333133991
+        image.Mutate(x => x.BackgroundColor(Color.White));
+
+        using var ms = new MemoryStream();
+        image.Save(ms, new JpegEncoder());
+
+        return ms.ToArray();
+    }
+}
